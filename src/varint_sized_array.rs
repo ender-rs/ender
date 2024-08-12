d6e6535824990157cfdbe2a5639ddc2784db7d10
Bruce0203 <BruceFreedy@gmail.com>
2024-08-13 08:37:05 +0900

    fix var int sized array capacity error

--- src/varint_sized_array.rs
+++ src/varint_sized_array.rs
@@ -4,7 +4,7 @@ use packetize::{Decode, Encode};
 
 use crate::var_int::VarInt;
 
-#[derive(derive_more::Deref, derive_more::DerefMut)]
+#[derive(Debug, derive_more::Deref, derive_more::DerefMut)]
 pub struct VarIntSizedArray<T, const N: usize>(ArrayVec<T, N>);
 
 impl<T, const N: usize> VarIntSizedArray<T, N> {
@@ -13,7 +13,7 @@ impl<T, const N: usize> VarIntSizedArray<T, N> {
     }
 }
 
-impl<T: Encode, const N: usize> Encode for VarIntSizedArray<T, N> {
+impl<T: Encode, const CAP: usize> Encode for VarIntSizedArray<T, CAP> {
     fn encode(&self, buf: &mut impl WriteBuf) -> Result<(), ()> {
         VarInt::from(self.len() as i32).encode(buf)?;
         for ele in self.iter() {
@@ -23,14 +23,19 @@ impl<T: Encode, const N: usize> Encode for VarIntSizedArray<T, N> {
     }
 }
 
-impl<T: Decode, const N: usize> Decode for VarIntSizedArray<T, N> {
+impl<T: Decode, const CAP: usize> Decode for VarIntSizedArray<T, CAP> {
     fn decode(buf: &mut impl fastbuf::ReadBuf) -> Result<Self, ()> {
         let mut arrayvec = VarIntSizedArray::new();
-        unsafe { arrayvec.set_len(*VarInt::decode(buf)? as usize) };
-        for _i in 0..arrayvec.len() {
-            arrayvec.push(T::decode(buf)?);
+        let len = *VarInt::decode(buf)? as usize;
+        if CAP < len {
+            #[cfg(debug_assertions)]
+            dbg!(CAP < len, CAP, len);
+            Err(())?
+        }
+        unsafe { arrayvec.set_len(len) };
+        for i in 0..arrayvec.len() {
+            *unsafe { arrayvec.get_unchecked_mut(i) } = T::decode(buf)?;
         }
         Ok(arrayvec)
     }
 }
-
